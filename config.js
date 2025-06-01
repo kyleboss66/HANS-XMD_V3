05aa7623c8b792e6b1dbd1db3a69d20a5ab7e938
MASTER KYLE <abdurazaki2007@gmail.com>
2025-06-01 13:15:32 +0300

    Update config.js

--- config.js
+++ config.js
@@ -8,15 +8,15 @@ const databasePath = path.join(__dirname, './database.db');
 const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
-module.exports = { session: process.env.SESSION_ID || '',
+module.exports = { session: process.env.SESSION_ID || 'HansTz&ewIARIhL#E5CFs0zeCqPJV1MPV7kCJiRKfRvE7YrVGIpc33r_M2s',
     PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "HANS-XMD",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || " 263717087382",              
+    OWNER_NAME: process.env.OWNER_NAME || "KYLE-BOY",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || " 255756886492",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
     BOT : process.env.BOT_NAME || 'HANS-XMD',
     URL : process.env.BOT_MENU_LINKS || 'https://files.catbox.moe/mn835l.jpg',
-    MODE: process.env.PUBLIC_MODE || "yes",
+    MODE: process.env.PUBLIC_MODE || "no",
     PM_PERMIT: process.env.PM_PERMIT || 'yes',
     HEROKU_APP_NAME : process.env.HEROKU_APP_NAME,
     HEROKU_APY_KEY : process.env.HEROKU_APY_KEY ,
